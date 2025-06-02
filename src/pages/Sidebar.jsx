51694ae927fa1e3c8f9d6893e9b485a58ccc9449
Aviek Paul <aviek17@gmail.com>
2025-06-02 22:47:05 +0530

    day-9

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -6,9 +6,7 @@ import Archive from "../BoxIcons/ArchiveIcon";
 import Favorite from "../BoxIcons/FavouriteIcon";
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import DonutLargeRoundedIcon from '@mui/icons-material/DonutLargeRounded';
-import { Divider } from '@mui/material';
-
-
+import { Divider, Drawer } from '@mui/material';
 
 
 const Sidebar = () => {
@@ -19,19 +17,33 @@ const Sidebar = () => {
     const bottomFunctionGroupIcons = [AccountCircleOutlinedIcon, Setting];
 
 
+
+    const onMenuClick = () => {
+        
+    }
+
+
     return (
-        <div style={{ height: `calc(100vh - 50px)` }} className='bg-[#ffffff] w-[50px] flex flex-col justify-between items-center'>
-            <div className='mt-[15px] flex flex-col gap-1.5'>
-                <SidebarIcon iconList={topGroupIcons} />
-                <SidebarIcon iconList={topFunctionalitiesGroupIcon} />
+        <>
+            <div style={{ height: `calc(100vh - 50px)` }} className='bg-[#ffffff] w-[50px] flex flex-col justify-between items-center'>
+                <div className='mt-[15px] flex flex-col gap-1.5'>
+                    <SidebarIcon iconList={topGroupIcons} onIconClick={onMenuClick} />
+                    <SidebarIcon iconList={topFunctionalitiesGroupIcon} />
+                </div>
+                <div className='mt-[15px] flex flex-col gap-1.5 mb-[15px]'>
+                    <SidebarIcon iconList={bottomChatGroupIcons} />
+                    <Divider sx={{ borderColor: '#005498' }} />
+                    <SidebarIcon iconList={bottomFunctionGroupIcons} />
+                </div>
             </div>
-            <div className='mt-[15px] flex flex-col gap-1.5 mb-[15px]'>
-                <SidebarIcon iconList={bottomChatGroupIcons} />
-                <Divider sx={{ borderColor: '#005498' }}/>
-                <SidebarIcon iconList={bottomFunctionGroupIcons} />
-            </div>
-        </div>
+
+            <Drawer open={false} onClose={onMenuClick}>
+                Drawer is opened
+            </Drawer>
+        </>
     )
 }
 
 export default Sidebar
+
+
