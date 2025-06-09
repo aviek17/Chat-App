2b92bf7a5cf8402420554d76abf56dafce3714cd
Aviek Paul <aviek17@gmail.com>
2025-06-10 02:04:06 +0530

    day-13

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,24 @@
-console.log("Entered in index js file")
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+//const user = require('./routes/user');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
+  
+
+//app.use('/user', user);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
