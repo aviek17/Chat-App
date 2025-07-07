98fd28bc933fbc4c3d9fe5d141da9cfa6c59aae6
Aviek Paul <aviek17@gmail.com>
2025-07-08 01:49:03 +0530

    day-20

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,17 @@
-import MainLayout from "./layouts/MainLayout"
-import Login from "./pages/Login"
-import Profile from "./pages/Profile"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <>
-      <MainLayout/>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </>
   )
 }
